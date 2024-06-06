7c4ef2d067f80bd41a4ce888bd403e93aaef0c95
gnobshobble <chickenprincessbride@gmail.com>
2024-06-06 10:57:33 -0700

    updated secret keys

--- myappBackend/settings.py
+++ myappBackend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kocg_2cq#tjl2utt85qrnr61=ot1q+f7&unpzrb(*x#+)2s#6l'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
